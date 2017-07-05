a2614b59583244a9ca4afd9747467b26161a4ab2
Amy Wilson <amy.liz@me.com>
2017-07-05 09:27:15 -0700

    completion of listener branch

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var heading = document.getElementById('header');
 
+var clicks = 0;
+function clickCounter() {
+    clicks += 1;
+}
+
 heading.addEventListener('click', function () {
+    clickCounter();
     var myElement = document.createElement('paragraph');
-    myElement.innerHTML = '<p>This is click "function(CountFun)" </p>';
+    myElement.innerHTML = '<p>This is click number</p>' + clicks;
     document.getElementById('header').appendChild(myElement);
 });
