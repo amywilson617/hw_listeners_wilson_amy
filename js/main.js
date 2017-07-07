cd90310fc54fbe97185d2f783390b895b1068d4f
Amy Wilson <amy.liz@me.com>
2017-07-07 12:52:35 -0700

    completion of event listeners

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ var fname = document.getElementById('fname');
 
 var lname = document.getElementById('lname');
 
-var email_address = document.getElementById('email_address');
+var emailaddress = document.getElementById('emailaddress');
+
+var comments = document.getElementById('comments');
 
 var submitbutton = document.getElementById('submit');
 submitbutton.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log(fname.value) (lname.value (email_address.value);
-};
+    console.log(fname.value);
+    console.log(lname.value);
+    console.log(emailaddress.value);
+    console.log(comments.value);
+});
